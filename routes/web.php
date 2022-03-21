dfc201d9137d0e7f42db6d9a9f407d951d6455be
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-03-21 19:14:48 +0100

    kleine korrekturen

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,18 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ArticleController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    Home-Routen
 */
+Route::redirect('/','/articles');
+Route::redirect('/home','/articles');
 
-Route::get('/', [HomeController::class,'getHome']);
-Route::get('/home', [HomeController::class,'getHome']);
+//Route::get('/', [HomeController::class,'getHome']);
+//Route::get('/home', [HomeController::class,'getHome']);
 
-//Artikelansicht
+
+/*
+    Artikel-Routen
+*/
 Route::get('/articles',[ArticleController::class,'showAllArticle']);
 Route::get('/articles/search',[ArticleController::class,'showArticle']);
 
