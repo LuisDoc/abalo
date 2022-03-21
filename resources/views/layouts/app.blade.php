33e93e4253b9dc187cc080cd69ac368a683393e1
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-03-21 16:34:57 +0100

    Navbar hinzugefügt

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!Doctype html>
+<html lang="DE">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Abalo</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</head>
+
+<body>
+    <!-- Navigationsleiste -->
+    <nav class="navbar-container">
+        <!--Logo -->
+        <div class="navbar-item">
+            <img id="navbarLogo" src="{{ url('/images/logo.jpg') }}" alt="Abalo">
+        </div>
+        <!-- Buttons -->
+        <div class="navbar-item">
+            <a class="btn btn-secondary" href="/home">Startseite</a>
+        </div>
+        <div class="navbar-item">
+            <a class="btn btn-secondary" href="/articles">Articles</a>
+        </div>
+    </nav>
+    <!-- Ende Navbar -->
+    @yield('content')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class,'getHome']);
+Route::get('/home', [HomeController::class,'getHome']);
+
+//Artikelansicht
+Route::get('/articles',[ArticleController::class,'showAllArticle']);
+
