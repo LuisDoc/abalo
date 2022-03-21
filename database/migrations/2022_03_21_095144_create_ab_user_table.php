2a412884b457d5491ab3306d48a388ecefb19d36
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-03-21 11:22:34 +0100

    Bearbeitung Aufgabe 7 Migrations
    Co-authored-by: Nilusche Liyanaarachchi <Nilusche@users.noreply.github.com>

---
+++ database/migrations/2022_03_21_095144_create_ab_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ab_user', function (Blueprint $table) {
+            $table->id();
+            $table->string('ab_name',80)->nullable(false);
+            $table->string('ab_password',200)->nullable(false);
+            $table->string('ab_mail',200)->nullable(false)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ab_user');
+    }
+};
