3aa57aeca1d9c33adb3a94d028cb2663dd15c5f1
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-03-23 12:25:47 +0100

    Redundanzen verhindert

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ab_article;
 
 class HomeController extends Controller
 {
     public function getHome(){
-        return view('index');
+        $articlePreview = ab_article::query()->orderBy('ab_createdate','desc')->limit(3)->get();
+        
+        return view('index')->with('articles',$articlePreview);
     }
 }
