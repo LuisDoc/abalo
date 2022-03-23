0fd72b0df675ff98d9173af9474f5e7f5f30764b
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-03-23 12:25:25 +0100

    Homepage zeigt neuste Artikel; Redundanzen verhindert

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,12 @@
 @section('content')
     <div class="container">
         <h1>Homepage</h1>
+        <!-- Anzeigen der drei neusten Artikel -->
+        <h3>Latest articles</h3>
+        <!-- Tabelle für Artikel importieren -->
+        @include('layout imports.articleTable')
+        <a href="/articles" class="defaultButton">Show More articles</a>
+
+
     </div>
 @endsection
