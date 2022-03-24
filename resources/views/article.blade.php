5929c1c8ed0bef1eaece3797c84a23cda9d2c20f
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-03-24 14:18:39 +0100

    test commit

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-
+<!---->
         <div class="search">
             <form action="/articles/search" method=" GET">
                 <input name="search" type="search" placeholder="Search" aria-label="Search">
