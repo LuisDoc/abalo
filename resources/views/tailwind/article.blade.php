55d1c074014481abc64bc002e91a46d0d98234bf
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-03-26 01:34:08 +0100

    Tabellenvariante hinzugefügt falls notwendig

--- resources/views/tailwind/article.blade.php
+++ resources/views/tailwind/article.blade.php
@@ -12,9 +12,9 @@
                         <div class="p-4">
                             <h1 class="headline font-bold mb-2 truncate text-purple">{{ $article->ab_name }}</h1>
                             <p class="text-sm mt-4 text-purple font-semibold mb-1">{{ $article->ab_preis }}</p>
-                            <p class="text-sm text-gray-600 mb-1 truncate">{{ $article->ab_description }}</p>
-                            <p class="text-sm text-gray-600 mb-1">Creator: {{ $article->ab_creator_id }}</p>
-                            <p class="text-sm text-gray-500 mb-1">{{ $article->ab_createdate }}</p>
+                            <p class="text-sm text-gray-600 mb-1 truncate"><b class="text-purple">About:</b> {{ $article->ab_description }}</p>
+                            <p class="text-sm text-gray-600 mb-1"><b class="text-purple">Creator:</b> {{ $article->ab_creator_id }}</p>
+                            <p class="text-sm text-gray-600 mb-1"><b class="text-purple">Created at:</b> {{ $article->ab_createdate }}</p>
                             @if (file_exists(public_path() . '/images/articlepictures/' . $article->id . '.jpg'))
                                 <img src="{{ url('/images/articlepictures/' . $article->id . '.jpg') }}" alt="" class="w-full h-60 object-cover mt-5">
                             @else
@@ -26,4 +26,39 @@
             </div>
         </div>
     </div>
+<!-- Tabellenvariante?
+<div class="bg-cgray">
+        <div class="text-6xl headline ml-64 pt-20 mb-20 text-purple">
+            Alle Artikel im Überblick
+        </div>
+        <div class=" p-64 pt-20">
+            <table>
+                <thead>
+                    <td scope="col">Bild</td>
+                    <td scope="col">Name</td>
+                    <td scope="col">Beschreibung</td>
+                    <td scope="col">Ersteller</td>
+                    <td scope="col">Datum</td>
+                </thead>
+                <tbody>
+                    @foreach ($articles as $article)
+                        <tr class="border border-black">
+                            <td>
+                                @if (file_exists(public_path() . '/images/articlepictures/' . $article->id . '.jpg'))
+                                    <img class="border border-black"src="{{ url('/images/articlepictures/' . $article->id . '.jpg') }}" alt="">
+                                @else
+                                    <img class="border border-black"src="{{ url('/images/articlepictures/' . $article->id . '.png') }}" alt="">
+                                @endif
+                            </td>
+                            <td>{{ $article->ab_name }}</td>
+                            <td>{{ $article->ab_description }}</td>
+                            <td>{{ $article->ab_creator_id }}</td>
+                            <td>{{ $article->ab_createdate }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+-->
 @endsection
