4d43d758070f4a282bdd22fc46ed728efdb0a49e
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-04-10 00:29:36 +0200

    Cookie-Settings UI Changes

--- resources/views/tailwind/Cookie/CookieSettings.blade.php
+++ resources/views/tailwind/Cookie/CookieSettings.blade.php
@@ -2,13 +2,13 @@
 @section('content')
 <div class ="mx-20 my-32">
     <h1 class="firstHeadline">Cookie Einstellungen</h1>
-    <div class="mx-64 rounded-xl">
-        <div class ="flex justify-center">
-            <button id="btnAcceptCookies" class ="">Cookies aktiviert</button>
-            <button id="btnRefuseCookies" class ="">Cookies deaktiviert</button>
+    <div class="mx-64 rounded-xl mt-32">
+        <div class ="flex justify-center  mb-64">
+            <button id="btnAcceptCookies" class ="btn mx-10 p-5 border-2 border-purple">Cookies aktiviert</button>
+            <button id="btnRefuseCookies" class ="btn mx-10 p-5 border-2 border-purple">Cookies deaktiviert</button>
         </div>
         <div class="flex justify-center">
-            <a class ="underline" href="/CookieGuidelines">Mehr Informationen</a>
+            <a class ="underline rounded-xl p-5 hover:border-2 hover:border-purple hover:text-purple transition ease-in-out duration-300" href="/CookieGuidelines">Mehr Informationen</a>
         </div>
     </div>
 </div>
