7f64368bd802803a1aba7ca504bba5977dacb350
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-04-10 00:54:39 +0200

    Impressum Added

--- public/js/ShoppingCart/viewShoppingCart.js
+++ public/js/ShoppingCart/viewShoppingCart.js
@@ -3,3 +3,15 @@
 //Search HTML Elements
 const anker = document.querySelector("#anker");
 
+let sessionData = sessionStorage.getItem(shoppingcartkey);
+
+articles.forEach((e)=>{
+    let articleID = e['id'];
+    //Iterieren durch Warenkorb
+    sessionData.forEach((e) =>{
+        
+    })
+    if(sessionData.includes(articleID)){
+        console.log(articleID + "liegt im Warenkorb");
+    }
+});
