3e44ec611b10d54f839d589980e9045be59cfceb
Luis Diniz Do Carmo <81914129+LuisDoc@users.noreply.github.com>
2022-05-04 12:17:47 +0200

    Check if shopping cart button is not null required

--- public/js/ShoppingCart/displayAddRemoveShoppingCart.js
+++ public/js/ShoppingCart/displayAddRemoveShoppingCart.js
@@ -1,3 +1,4 @@
+console.log("ShoppingCart Button Script Started");
 //Daten über Artikel im Shopping Cart besorgen
 let xhr = new XMLHttpRequest();
 let params = "";
@@ -19,11 +20,13 @@ function displayButtonCorrectly (items){
     
     let red = "w-14 h-15 rounded-full p-2 border-2 border-red-400 hover:bg-red-400 transition duration-300 ease-out";
 
+    console.warn("Artikel die sich bereits im Warenkorb befinden:");
     items.forEach((item)=>{
-        console.log("Artikel mit der ID " + item['id'] + " befindet sich bereits im Warenkorb");
+        console.log("Artikel mit der ID " + item['ab_article_id'] + " befindet sich bereits im Warenkorb");
         let articleID = item['ab_article_id'];
         let btn = document.querySelector("#addShoppingCart"+articleID);
-        console.log(btn);
-        btn.setAttribute("class",red);
+        if(btn != null){
+            btn.setAttribute("class",red);
+        }
     });
 }
